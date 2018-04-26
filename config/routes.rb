0716f61b2dae31e78e3c6ce3042e8510dfdf9767
“devinlewtan” <devinlewtan@gmail.comssh-keygen -t rsa -C devinlewtan@gmail.com>
2018-04-26 16:11:05 +0200

    added:Page layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/login', to: 'static_pages#login'
+  get  '/signup',  to: 'users#new'
 end
