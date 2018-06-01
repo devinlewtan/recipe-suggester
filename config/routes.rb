24f35b593b8230b5431851c181c85b665acb55eb
“devinlewtan” <devinlewtan@gmail.comssh-keygen -t rsa -C devinlewtan@gmail.com>
2018-06-01 14:03:22 -0400

    Implemented log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
-  
-  resources :ingredients
+
 
   root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/login', to: 'static_pages#login'
+  get '/index', to: 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   resources :users
 end
